ff926cdd5f9cc4d6d8422a32bc39ca6790f1e06a
teclilla <pablito.fa.pas@gmail.com>
2013-05-24 11:55:35 -0300

    -mmm

---
+++ src/test/java/com/unrc/app/OwnerSpec.java
@@ -0,0 +1,66 @@
+package com.unrc.app;
+
+import static org.javalite.test.jspec.JSpec.*;
+
+import org.javalite.activejdbc.Base;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.unrc.app.models.City;
+import com.unrc.app.models.Owner;
+
+public class OwnerSpec {
+    @Before
+    public void before(){
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development_test", "root", "");
+        Base.openTransaction();
+    }
+    
+    @After
+    public void after(){
+     Base.rollbackTransaction();
+        Base.close();
+    }
+    
+    @Test
+    public void shouldValidateMandatoryFields(){
+        Owner owner = new Owner();
+
+        //check errors
+        the(owner).shouldNotBe("valid");
+        the(owner.errors().get("first_name")).shouldBeEqual("Name");
+        the(owner.errors().get("last_name")).shouldBeEqual("LastName");
+        the(owner.errors().get("email")).shouldBeEqual("Email");
+
+        //all is good:
+        owner.set("first_name","Homer");
+        owner.set("last_name","Simpson");
+        owner.set("email","homersimpson@fox.com");
+        
+        the(owner).shouldBe("valid");
+    }
+    
+   @Test
+    public void shouldSaveRecord(){
+        Owner owner = new Owner();
+        City city = new City();
+    
+     city.set("name", "space").saveIt();
+        
+        owner.set("first_name","Lisa");
+        owner.set("last_name","Simpson");
+        owner.set("city_id",city.getId());
+        owner.set("neighborhood", "Bn");
+        owner.set("street", "st_Bn");
+        owner.set("n_street", "111");
+        owner.set("email","lisasimpson@fox.com");
+        owner.saveIt();
+        
+        Owner o = Owner.findFirst("first_name = ?", "Homer");
+        the(owner.getId()).shouldBeEqual(o.getId());
+    }
+}
+
+
+
